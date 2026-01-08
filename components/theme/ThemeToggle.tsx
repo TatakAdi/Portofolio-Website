7d5f8b9172b5665a22fb70877f996d7a62446659
TatakAdi <tatakadisubagyo@gmail.com>
2026-01-09 03:23:42 +0800

    feat: Hover toggle theme button & Page color transition

--- components/theme/ThemeToggle.tsx
+++ components/theme/ThemeToggle.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import { Sun, Moon } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+
+  const isDark = theme === "dark";
 
   useEffect(() => {
     setMounted(true);
@@ -15,10 +18,12 @@ export default function ThemeToggle() {
 
   return (
     <button
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="rounded-md border px-3 py-2 text-sm cursor-pointer"
+      onClick={() => setTheme(isDark ? "light" : "dark")}
+      className="
+      flex items-center gap-2 rounded-md border px-3 py-2 text-sm cursor-pointer transition-colors duration-200 ease-in-out bg-(--background) text-(--foreground) hover:bg-(--foreground) hover:text-(--background)"
     >
-      {theme === "dark" ? "🌙 Dark" : "☀️ Light"}
+      {isDark ? <Moon /> : <Sun />}
+      <span>{isDark ? "Dark" : "Light"}</span>
     </button>
   );
 }
