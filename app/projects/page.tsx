81e9da1e9c2941b0d168381d069c5b8170ceb4ef
TatakAdi <tatakadisubagyo@gmail.com>
2026-01-12 02:30:42 +0800

    feat: UseInView for Project Section & Change website title for web tab

---
+++ app/projects/page.tsx
@@ -0,0 +1,3 @@
+export default function ProjectsPage() {
+  return <div className="min-h-screen  px-6 py-8"></div>;
+}
--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -1,12 +1,17 @@
+"use client";
+
 import { Project, ProjectCardData } from "@/lib/types";
 import ProjectCard from "@/components/project/ProjectCard";
 import Link from "next/link";
+import { useInView } from "@/hooks/useInView";
 
 type ProjectProps = {
   projects: Project[];
 };
 
 export default function Projects({ projects }: ProjectProps) {
+  const { ref, isVisible } = useInView();
+
   const projectCards: ProjectCardData[] = projects.map(
     ({
       id,
@@ -32,8 +37,15 @@ export default function Projects({ projects }: ProjectProps) {
   );
 
   return (
-    <section id="projects" className="space-y-6">
-      <div className="flex items-center justify-between">
+    <section
+      id="projects"
+      className="space-y-6 transition-all duration-700 ease-out"
+    >
+      <div
+        className={`flex items-center justify-between transition-all duration-700 ease-out ${
+          isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-6"
+        }`}
+      >
         <h2 className="text-2xl font-bold">Projects</h2>
         <Link
           href="/projects"
@@ -42,7 +54,12 @@ export default function Projects({ projects }: ProjectProps) {
           View All
         </Link>
       </div>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div
+        ref={ref}
+        className={`grid gap-6 sm:grid-cols-2 lg:grid-cols-3 transition-all duration-700 ease-out ${
+          isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-8"
+        }`}
+      >
         {projectCards.slice(0, 3).map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
